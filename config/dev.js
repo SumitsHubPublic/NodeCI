69beb597a0b37fa0be8e9383d26e1091ce25e18c
Sumit Paul <pauls1317@gmail.com>
2023-11-07 12:38:05 +0530

    hide mongo uri

--- config/dev.js
+++ config/dev.js
@@ -2,8 +2,7 @@ module.exports = {
   googleClientID:
     '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
   googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
-  mongoURI:
-    'mongodb://thesumitpaul1998:dtWzFs9QvRoNA5DN@ac-l7batk4-shard-00-00.ahfacxd.mongodb.net:27017,ac-l7batk4-shard-00-01.ahfacxd.mongodb.net:27017,ac-l7batk4-shard-00-02.ahfacxd.mongodb.net:27017/blog-app?ssl=true&replicaSet=atlas-zijwkh-shard-0&authSource=admin&retryWrites=true&w=majority',
+  mongoURI: process.env.MONGO_URI,
   cookieKey: '123123123',
   redisUrl: 'redis://127:0.0.6379',
 };
